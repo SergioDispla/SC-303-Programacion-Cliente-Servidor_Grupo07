b837518f5e1c404156735e1c03314faa1dd23f41
Sergio Monge <82247215+SergioDispla@users.noreply.github.com>
2024-02-12 17:48:28 -0600

    Update SC303ProgramacionClienteServidor_Grupo07.java

--- src/main/java/principal/SC303ProgramacionClienteServidor_Grupo07.java
+++ src/main/java/principal/SC303ProgramacionClienteServidor_Grupo07.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package principal;
 
-/**
- *
- * @author sams2
- */
 public class SC303ProgramacionClienteServidor_Grupo07 {
 
     public static void main(String[] args) {
