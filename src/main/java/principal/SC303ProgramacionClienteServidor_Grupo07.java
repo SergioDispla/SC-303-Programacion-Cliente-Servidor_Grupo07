fccaa6bf523beebbc92914101de52c23495d1a2b
Sergio Monge <82247215+SergioDispla@users.noreply.github.com>
2024-02-12 17:46:09 -0600

    Initial commit

---
+++ src/main/java/principal/SC303ProgramacionClienteServidor_Grupo07.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package principal;
+
+/**
+ *
+ * @author sams2
+ */
+public class SC303ProgramacionClienteServidor_Grupo07 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
