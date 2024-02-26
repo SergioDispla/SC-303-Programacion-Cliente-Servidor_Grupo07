99fb61f60d5759ace97c0c11d30108938946fe54
Sergio Monge <82247215+SergioDispla@users.noreply.github.com>
2024-02-25 21:46:46 -0600

    Modificacion clase operador y administrador

--- src/main/java/Personas/Administrador.java
+++ src/main/java/Personas/Administrador.java
@@ -1,11 +1,61 @@
 package Personas;
 
 public class Administrador extends Persona {
-    
-    public Administrador(String cedula, String nombre, String telefono, 
-            String direccion, String correoElectronico, roles rolUsuario) {
-        super(cedula, nombre, telefono, direccion, correoElectronico, 
-                rolUsuario.Administrador);
+    private String idEmpleado;
+    private String usuario;
+    private String contrasena;
+
+    public Administrador(String idEmpleado, String usuario, String contrasena, String cedula, String nombre, String telefono, String direccion, String correoElectronico, roles rolUsuario) {
+        super(cedula, nombre, telefono, direccion, correoElectronico, rolUsuario.Administrador);
+        this.idEmpleado = idEmpleado;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
     }
+
+    public String getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(String idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public roles getRolUsuario() {
+        return rolUsuario;
+    }
+
+    public void setRolUsuario(roles rolUsuario) {
+        this.rolUsuario = rolUsuario;
+    }
+
+    @Override
+    public void informacionPersona(){
+        System.out.println("Cédula: "+getCedula());
+        System.out.println("ID de Empleado: "+idEmpleado);
+        System.out.println("Nombre: "+getNombre());
+        System.out.println("Teléfono: "+getTelefono());
+        System.out.println("Dirección: "+getDireccion());
+        System.out.println("Correo Electrónico: "+getCorreoElectronico());
+        System.out.println("Usuario de Sistema: "+usuario);    
+        System.out.println("Contraseña de Sistema: "+contrasena);
+
+        
+    }   
     
 }
--- src/main/java/Personas/Operador.java
+++ src/main/java/Personas/Operador.java
@@ -1,11 +1,59 @@
 package Personas;
 
+import java.time.LocalDate;
+
 public class Operador extends Persona {
-    
-    public Operador(String cedula, String nombre, String telefono, 
-            String direccion, String correoElectronico, roles rolUsuario) {
-        super(cedula, nombre, telefono, direccion, correoElectronico, 
-                rolUsuario.Operador);
+    private String idEmpleado;
+    private LocalDate fechaContratacion;
+    private float salario;
+
+    public Operador(String idEmpleado, LocalDate fechaContratacion, 
+            float salario, String cedula, String nombre, 
+            String telefono, String direccion, String correoElectronico, 
+            roles rolUsuario) {
+        super(cedula, nombre, telefono, direccion, correoElectronico, rolUsuario.Operador);
+        this.idEmpleado = idEmpleado;
+        this.fechaContratacion = fechaContratacion;
+        this.salario = salario;
     }
+
+    public String getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(String idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public LocalDate getFechaContratacion() {
+        return fechaContratacion;
+    }
+
+    public void setFechaContratacion(LocalDate fechaContratacion) {
+        this.fechaContratacion = fechaContratacion;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+    
+    
+    @Override
+    public void informacionPersona(){
+        System.out.println("Cédula: "+getCedula());
+        System.out.println("ID de Empleado: "+idEmpleado);
+        System.out.println("Nombre: "+getNombre());
+        System.out.println("Teléfono: "+getTelefono());
+        System.out.println("Dirección: "+getDireccion());
+        System.out.println("Correo Electrónico: "+getCorreoElectronico());
+        System.out.println("Tipo Usuario: "+getRolUsuario());    
+        System.out.println("Fecha de Contratación: "+fechaContratacion);
+        System.out.println("Salario devengado: "+salario);
+        
+    }    
     
 }
